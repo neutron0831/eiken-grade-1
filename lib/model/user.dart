73a77f47908937579e6a77b246163cd2d21ea2d8
NEUTRON <neutron0831@gmail.com>
2022-07-09 22:33:47 +0900

    🏷️ Add User and Word class

---
+++ lib/model/user.dart
@@ -0,0 +1,7 @@
+class User {
+  String id;
+  String username;
+  List? words;
+
+  User({required this.id, required this.username, this.words});
+}
---
+++ lib/model/word.dart
@@ -0,0 +1,34 @@
+class Word {
+  String uuid;
+  String category;
+  String eng;
+  String exEng;
+  String exJap;
+  String? exp;
+  String id;
+  String jap;
+  String level;
+  String mp3Eng;
+  String mp3Ex;
+  String mp3Jap;
+  String no;
+  String? pron;
+  String pum;
+
+  Word(
+      {required this.uuid,
+      required this.category,
+      required this.eng,
+      required this.exEng,
+      required this.exJap,
+      this.exp,
+      required this.id,
+      required this.jap,
+      required this.level,
+      required this.mp3Eng,
+      required this.mp3Ex,
+      required this.mp3Jap,
+      required this.no,
+      this.pron,
+      required this.pum});
+}
