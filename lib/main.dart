cdcc5a37f6b35c0496e9db2d2eec2011a0f6ee90
neutron0831 <neutron0831@gmail.com>
2022-07-11 11:14:41 +0900

    :card_file_box: Add Cloud Firestore plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:eiken_grade_1/firebase_options.dart';
 import 'package:eiken_grade_1/pages/top_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
