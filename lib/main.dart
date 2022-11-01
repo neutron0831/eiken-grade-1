c8e614d076ecb879cb17c44de695c658129f5c22
neutron0831 <neutron0831@gmail.com>
2022-11-02 02:44:40 +0900

    :speech_balloon: Update app title #30
    Change app title to 'Eiken Grade 1'.

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Eiken Grade 1',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
